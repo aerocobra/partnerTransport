e153fceff60a87d73d2393504728e9af1b8a3ee5
root <root@ip-172-31-40-198.eu-west-1.compute.internal>
2017-05-03 20:11:46 +0000

    last

--- __openerp__.py
+++ __openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017 Igor V. Kartashov <nedas.zilinskas@gmail.com>
+# © 2017 Igor V. Kartashov
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": 'partner transport data',
