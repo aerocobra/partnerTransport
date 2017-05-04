7d4af0c4c9c99f5d8d082fdcde537b4a738b005e
root <root@ip-172-31-40-198.eu-west-1.compute.internal>
2017-05-04 21:23:47 +0000

    job positions

---
+++ models/transportJobPositions.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+# -*- transportJobPositions.py
+from openerp import tools
+from openerp import models, fields, api
+
+class transportJobPositions ( models.Model):
+	_name		= "transport.job.positions"
+	_rec_name	= "x_strJobPosition" #IMPORTANTE - por este campo se hace la selección poe defecto en el formulario
+	
+	x_strJobPosition	= fields.Char ( string = "Cargo")
